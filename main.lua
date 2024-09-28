eabb917fda8a8831f15416318aff14e00e852489
sawby <130822964+bradensMG@users.noreply.github.com>
2024-09-28 14:11:09 -0400

    seperate file for fpslimiter

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
-local FPS = 30
 local debugMode = true
 
 require 'source.errhand'
+require 'source.fpsLimiter'
 
 function reload()
 	love.audio.stop()
@@ -114,58 +114,5 @@ function love.draw()
 		love.graphics.setFont(fonts.consolas)
 		love.graphics.setColor(1, 1, 1)
 		love.graphics.print('FPS: ' .. love.timer.getFPS() .. '\ngameState: ' .. global.gameState .. '\nbattleState: ' .. global.battleState .. '\nRAM Usage: ' .. math.floor(collectgarbage("count")) .. ' KB', 10, 10)
-	end
-end
-
-local timerSleep = function () return 1/FPS end
-function love.run()
-	if love.math then
-		love.math.setRandomSeed(os.time())
-	end
- 
-	if love.load then love.load(arg) end
- 
-	-- We don't want the first frame's dt to include time taken by love.load.
-	if love.timer then love.timer.step() end
- 
-	local dt = 0
- 
-	-- Main loop time.
-	while true do
-		-- Process events.
-		local startT = love.timer.getTime()
-		
-		if love.event then
-			love.event.pump()
-			for name, a,b,c,d,e,f in love.event.poll() do
-				if name == "quit" then
-					if not love.quit or not love.quit() then
-						return a
-					end
-				end
-				love.handlers[name](a,b,c,d,e,f)
-			end
-		end
- 
-		-- Update dt, as we'll be passing it to update
-		if love.timer then
-			love.timer.step()
-			dt = love.timer.getDelta()
-		end
- 
-		-- Call update and draw
-		if love.update then love.update(dt) end -- will pass 0 if love.timer is disabled
- 
-		if love.graphics and love.graphics.isActive() then
-			love.graphics.clear(love.graphics.getBackgroundColor())
-			love.graphics.origin()
-			if love.draw then love.draw() end
-			love.graphics.present()
-		end
- 
-		if love.timer then
-			local endT = love.timer.getTime()
-			love.timer.sleep(timerSleep() - (endT - startT))
-		end
 	end
 end
